6c8845a80f7cf1d48cc1ba0be8bf1fa13cedb8f6
Gabriel Castilho <castilho.gabriel14@gmail.com>
2019-03-11 17:13:07 -0300

    Work done in 2019-03-11
    --Client: O JSON enviado agora contem um objeto 'data' com todas as informações de telemetria e uma propriedade 'signature', que agoira fica fora do que anteriormente era o data
    --Client: Signature agora contêm o hash do data com a API Key
    --Server: Adicionado função para buscar a API Key de um RA específico
    --SQL: Adicionado arquivo SQL com a estrutura do DB a ser usado
    --Minor changes

---
+++ databaseStructure.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE telemetryProject;
+use telemetryProject;
+
+CREATE TABLE Credentials (
+	RA bigint NOT NULL UNIQUE,
+	Api_Key varchar(30) NOT NULL,
+	PRIMARY KEY (RA)
+);
+
+CREATE TABLE Telemetry (
+	RA bigint NOT NULL,
+	timestamp int(10) NOT NULL,
+	Latitude varchar(11) NOT NULL,
+	Longitude varchar(11) NOT NULL,
+	windVelocity float(5),
+
+	FOREIGN KEY(RA)	REFERENCES credentials(RA)
+);
--- telemetry.php
+++ telemetry.php
@@ -1,15 +1,44 @@
 <?php
 	$min_version = 0.1;
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$data = json_decode(file_get_contents("php://input"));
-		if ($data->version < $min_version) {
+		$input = json_decode(file_get_contents("php://input"));
+		if ($input->data->version < $min_version) {
 			echo "Min protocol version is $min_version";
 			return;
 			//replace with sending ONLY a specific status code
 		}
-		//print_r($data);
+		print_r($input);
+		getApiKey($input->data->RA);
 		//check if signature is valid from given username before inserting into DB
-		appendTelemetry($data->RA, $data->telemetry);
+		//appendTelemetry($input->data->RA, $input->data->telemetry);
+	}
+
+	function getApiKey($RA) {
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "telemetryProject";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$sql = "SELECT Api_Key FROM Credentials WHERE RA = $RA;";
+		
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				echo "API_Key: " . $row["Api_Key"]. "\n";
+			}
+		} else {
+			echo "0 results";
+		}
+		$conn->close();
 	}
 
 	//function to verify signature from username (username might be a pubKey) stored in specific table
@@ -18,7 +47,7 @@
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
-		$dbname = "fatec_campinas";
+		$dbname = "telemetryProject";
 
 		// Create connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
